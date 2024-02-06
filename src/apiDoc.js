2051c3cbde3848a9bdee4b3e08123fd15222dc02
Christian Santanas <50605403+chris171290@users.noreply.github.com>
2024-02-06 15:58:36 -0500

    fix(swagger): :lipstick: add cdn to swagger ui

--- src/apiDoc.js
+++ src/apiDoc.js
@@ -2,6 +2,7 @@ import swaggerUI from 'swagger-ui-express'
 import { readFile } from 'fs/promises'
 
 const CSS_URL = '/public/swagger-ui2.css'
+const CDN_URL_CSS = 'https://cdn.jsdelivr.net/npm/swagger-ui-dist@latest/swagger-ui.css'
 const FAVICON_URL = '/public/favicon-32x32.png'
 
 const swaggerJson = JSON.parse(await readFile(new URL('./swagger.json', import.meta.url)))
@@ -9,7 +10,7 @@ const swaggerJson = JSON.parse(await readFile(new URL('./swagger.json', import.m
 const swaggerUIOptions = {
   explorer: true,
   customCss: '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
-  customCssUrl: CSS_URL,
+  customCssUrl: CDN_URL_CSS,
   customfavIcon: FAVICON_URL,
   swaggerOptions: {
     // deepLinking: true,
