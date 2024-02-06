1f5426789b30ec7e0ea491d9ad9299977fcd909e
Christian Santanas <50605403+chris171290@users.noreply.github.com>
2024-02-06 16:00:25 -0500

    fix(eslint): fix eslint

--- src/apiDoc.js
+++ src/apiDoc.js
@@ -1,7 +1,7 @@
 import swaggerUI from 'swagger-ui-express'
 import { readFile } from 'fs/promises'
 
-const CSS_URL = '/public/swagger-ui2.css'
+// const CSS_URL = '/public/swagger-ui2.css'
 const CDN_URL_CSS = 'https://cdn.jsdelivr.net/npm/swagger-ui-dist@latest/swagger-ui.css'
 const FAVICON_URL = '/public/favicon-32x32.png'
 
